067a620b8c3ee765d29eb5d5cd52a58d98d0e46b
Jivyansh <jivyanshprajapat07@gmail.com>
2025-05-08 21:02:06 +0530

    Add files via upload

---
+++ Assignment 2/Practical/Q26.cpp
@@ -0,0 +1,9 @@
+// Write a C++ program that prints "Hello, World!" using the main function.
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World!";
+    return 0;
+}
---
+++ Assignment 2/Practical/Q45.cpp
@@ -0,0 +1,21 @@
+// Implement a function with default arguments to print a greeting message (default name is guest).
+#include<iostream>
+
+using namespace std;
+// Enter Default argument in prototype not in defination
+void greeting(string name = "Guest");
+
+int main(){
+	string name;
+	getline(cin , name);
+	if(name.empty()){
+		greeting();
+	}else{
+		greeting(name);
+	}
+	return 0;
+}
+
+void greeting(string name ){
+	cout<<"Hello "<<name<<endl;
+}
